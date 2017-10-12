a6511977537164a9c96a95601b182bc7cd8346d7
Student <student@SETASs-Mac-mini-2.local>
2017-10-11 21:50:15 -0400

    Dependent Picker

--- MAPD714-F2017-PickersTab/DependentComponentPickerViewController.swift
+++ MAPD714-F2017-PickersTab/DependentComponentPickerViewController.swift
@@ -1,35 +1,71 @@
-//
-//  DependentComponentPickerViewController.swift
-//  MAPD714-F2017-PickersTab
-//
-//  Created by Student on 2017-10-11.
-//  Copyright © 2017 Rafael Matos. All rights reserved.
-//
-
 import UIKit
 
-class DependentComponentPickerViewController: UIViewController {
+class DependentComponentPickerViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    private let stateComponent = 0
+    private let zipComponent = 1
+    private var stateZips:[String: [String]]!
+    private var states:[String]!
+    private var zips:[String]!
+    
+    @IBOutlet weak var dependentPicker: UIPickerView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        let bundle = Bundle.main
+        let plistURL = bundle.url(forResource: "statedictionary", withExtension: "plist")
+        stateZips = NSDictionary.init(contentsOf: (plistURL)!) as! [String: [String]]
+        let allStates = stateZips.keys
+        states = allStates.sorted()
+        let selectedState = states[0]
+        zips = stateZips[selectedState]
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func onButtonPressed(_ sender: UIButton) {
+        let stateRow = dependentPicker.selectedRow(inComponent: stateComponent)
+        let zipRow = dependentPicker.selectedRow(inComponent: zipComponent)
+        
+        let state = states[stateRow]
+        let zip = zips[zipRow]
+        
+        let title = "You selected zip code \(zip)"
+        let message = "\(zip) is in \(state)"
+        
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
     }
-    */
-
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 2
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return (component == stateComponent) ? states.count : zips.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return (component == stateComponent) ? states[row] : zips[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        if component == stateComponent {
+            let selectedState = states[row]
+            zips = stateZips[selectedState]
+            dependentPicker.reloadComponent(zipComponent)
+            dependentPicker.selectRow(0, inComponent: zipComponent, animated: true)
+        }
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, widthForComponent component: Int) -> CGFloat {
+        let pickerWidth = pickerView.bounds.size.width
+        return (component == zipComponent) ?pickerWidth/3 : 2 * pickerWidth/3
+    }
 }
